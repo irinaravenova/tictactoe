c7d9ec945518c7302da3def886fd86f562198325
irinaravenova <hernandezirina23@gmail.com>
2023-09-30 12:34:16 -0400

    populate cell with player choices on button click and switch players

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const gameBoard = (() => {
     // gameBoard object - use module
     // store gameboard as an array inside of Gameboard object
 
-    const gameboard = [["", "", ""], ["", "",""], ["", "", ""]]; 
-
     const getBoard = () => {
+
+    let gameboard = [["", "", ""], ["", "",""], ["", "", ""]]; 
 
     let board = document.querySelector('.gameboard');
 
@@ -29,6 +29,7 @@ const gameBoard = (() => {
         }
     }
 
+    
     return { getBoard };
     
 })();
@@ -37,54 +38,102 @@ const gameBoard = (() => {
 // Will encapsulate all the things our players can do (functions)
 
 const Player = (() => {
+    
+    // Create current player variable
+    let currentPlayer;
 
     let form = document.getElementById("myForm");
-    let listPlayers = document.getElementById("listPlayers");
-    let listButton = document.querySelector("players");
+    let players = document.querySelector(".players");
 
-    // Get player names from form input
+    // Get player names from form input 
     const listenForPlayers = () => {    
         form.addEventListener('submit', (e) => {
             e.preventDefault();
+            let buttonEl = e.target.children.submit;
+            buttonEl.innerHTML = "Reset Game"
             let player1 = document.getElementById("nameA").value;
             let player2 = document.getElementById("nameB").value;
-            result = [ player1, player2 ];
-            console.log(result);
+            playersArray = [ player1, player2 ];
 
-            for (player in result) {
-                let players = document.createElement("p");
-                players.innerHTML = `${result[player]}`;
-                listPlayers.appendChild(players);
+            currentPlayer = player1;
+            console.log(playersArray);
+            console.log(currentPlayer);
+
+            // List player names on screen
+            for (player in playersArray) {
+                let newPlayers = document.createElement("p");
+                newPlayers.innerHTML = `${playersArray[player]}`;
+                players.appendChild(newPlayers);
             }
 
-            return result;
-        });
+            return playersArray;
+
+        }, {once : true} );
     }
 
+    // Switch between players
+    const switchPlayers = () => {
+
+        let cell = document.querySelectorAll(".cell");
+
+        cell.forEach((cell) => {
+            cell.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                // Populate cell with player choice on click
+                if ( currentPlayer == playersArray[0] ) {
+                    e.target.innerHTML = "X";
+                }
+                else if ( currentPlayer == playersArray[1] ) {
+                    e.target.innerHTML = "O";
+                }
+
+                // Switch players on click of cell
+                ( currentPlayer == playersArray[0] ) ? 
+                ( currentPlayer = playersArray[1] ) : 
+                ( currentPlayer = playersArray[0] );
+
+                console.log(currentPlayer);
+                displayController.checkWinner();
+                return currentPlayer;
+            })
+        })
 
 
-    return { listenForPlayers };
+    }
+
+    return { listenForPlayers, switchPlayers };
 
 })();
 
 
 const displayController = (() => {  
-    // object to control flow of game
+    // Object to control flow of game
 
-    // Add cell click event listeners
-    // each click, po
+    // let updatedGameboard = [["", "", ""], ["", "",""], ["", "", ""]]; 
+
+    // Function comparing cell contents to check for winner
+    const checkWinner = () => {
+        let cell = document.querySelectorAll(".cell");
+
+        cell.forEach((cell) => {
+            console.log(cell.id)
+        })
+     
+    }
 
 
-
+    return { checkWinner };
 
 })();
 
 
 
-Player.listenForPlayers();
+
 gameBoard.getBoard();
-
-
+Player.listenForPlayers();
+Player.switchPlayers();
+displayController.checkWinner();
 
 
 
